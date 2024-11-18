77f193dd6c9a85a28cb1b7269f4f469f80cc065c
Paulo Fernandes <paulor.exe@gmail.com>
2024-11-18 09:04:48 -0300

    feat: add email validation on user creation

--- api/src/main/java/com/taskflow/api/repositories/UserRepository.java
+++ api/src/main/java/com/taskflow/api/repositories/UserRepository.java
@@ -2,8 +2,12 @@ package com.taskflow.api.repositories;
 
 import com.taskflow.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    public User findByEmailAddress(String email);
 }
